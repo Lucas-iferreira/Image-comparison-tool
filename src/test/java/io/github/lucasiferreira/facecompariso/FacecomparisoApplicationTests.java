e034fd619b021b21ce6305052e60f6654972169d
Lucas <lucas.iferreir@gmail.com>
2025-05-30 10:40:27 -0300

    First commit

---
+++ src/test/java/io/github/lucasiferreira/facecompariso/FacecomparisoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.lucasiferreira.facecompariso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacecomparisoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
