3d293793aef8745d620339012edac6645df1dadb
Lucas <lucas.iferreir@gmail.com>
2025-06-01 20:00:41 -0300

    fix test spring

--- src/test/java/io/github/lucasiferreira/facecomparison/FacecomparisonApplicationTests.java
+++ src/test/java/io/github/lucasiferreira/facecomparison/FacecomparisonApplicationTests.java
@@ -1,13 +1,20 @@
 package io.github.lucasiferreira.facecomparison;
 
 import io.github.lucasiferreira.facecomparison.config.FacecomparisonApplication;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(classes = FacecomparisonApplication.class)
-@ActiveProfiles(value = "test")
 class FacecomparisonApplicationTests {
+
+    @BeforeAll
+    static void setupProperties(){
+        System.setProperty("aws.region", "us-east-1");
+        System.setProperty("aws.accessKeyId", "localstack");
+        System.setProperty("aws.secretAccessKey", "localstack");
+    }
 
     @Test
     void contextLoads() {
